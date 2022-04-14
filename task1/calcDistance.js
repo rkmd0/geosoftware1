03879ffadd74936d3acf5676c6b5e7b558dacedc
rkmd0 <86024844+rkmd0@users.noreply.github.com>
2022-04-14 22:09:30 +0200

    Update to 0.0.6
    Commenting, improving code,...

--- task1/calcDistance.js
+++ task1/calcDistance.js
@@ -1,49 +1,64 @@
 "use strict"
+/**
+ * Geosoftware Abgabe 1
+ * @author Erkam Dogan
+ * @version 0.0.6
+ */
 
-//console.log(cities[1]) //gustav meeeow
+
+//var's for 
 var distancemeasurment = Array.apply(null, Array[cities.length]); // "temp" Array for the unsorted result of "calcDistanc"
 var finishedcalc = ""; //creating a, for now, undefined var for future purposes
+
+var longitude1 = point[0];
+var latitude1 = point[1];
 
 function distanceCalculator() {
   
 
-  var longitude1 = point[1];
-  var latitude1 = point[0];
+  for(var i = 0; i < cities.length; i++){
 
-  for(var i = 0; i < cities.lenght; i++){
-
-    var latitude2 = cities[i][1];
+    var latitude2 = cities[i][1]; //declaration of the lat and long points to start calc. the distance
     var longitude2 = cities[i][0];
     
-    
-    const R = 6371e3; //radius of earth in meter
+    //iterating to calc. to calc. the distance for each city
+    const R = 6371; //radius of earth in meter
     const φ1 = latitude1 * Math.PI/180; // φ, λ in radians
     const φ2 = latitude2 * Math.PI/180;
     const Δφ = (latitude2 - latitude1) * Math.PI/180;
     const Δλ = (longitude2 - longitude1) * Math.PI/180;
     const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ2) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    const d = R * c; // in metres
+    const d = Math.round((R * c) * 10) / 10;; //rounding up to get "better" values
 
     distancemeasurment[i] = d; //measuring the distances, array isnt sorted though
     
-
-    console.log(distancemeasurment[i]);
-
-
     
   }
- 
+
+  //function to sort array with insertionsort -> source: https://www.section.io/engineering-education/sorting-algorithms-in-js/#insertion-sort
+  function insertionSort(arr){
+    for(let i = 1; i < arr.length;i++){
+        for(let j = i - 1; j > -1; j--){
+            if(arr[j + 1] < arr[j]){
+                [arr[j+1],arr[j]] = [arr[j],arr[j + 1]];
+
+            }
+        }
+    };
+
+  return arr;
+}
     
-distancemeasurment.sort(function(a, b){ //sorting the
-  return b - a});
 
 
 
-  for(var k=0; k<distancemeasurment.length; k++){
 
-    finishedcalc = finishedcalc + distancemeasurment[k] + "<br />";
+  // iterating through the array to add it as text with breakpoints
+  for(var i2=0; i2<distancemeasurment.length; i2++){
+
+    finishedcalc = finishedcalc + distancemeasurment[i2] + " km" + "<br />";
   }
 
 }
-distanceCalculator();
+distanceCalculator(); //starting the method
